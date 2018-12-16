ca40977fb00f3bf5e8d23c1fa45172a7f4a56342
Luca Padovani <luca@l3p.it>
2018-12-16 14:34:35 +0000

    initial commit

---
+++ src/components/Pnl.vue
@@ -0,0 +1,157 @@
+<template lang="html">
+  <ul class="collapsible expandable">
+    <li>
+        <div class="collapsible-header red darken-1 white-text text-darken-2 left-align"><b>STRAT1</b><span style="margin-left:20px">-0.30%</span></div>
+        <div class="collapsible-body">
+          <div class="container">
+            <div class="row">
+              <div class="col s12 left-align" style="font-family: monospace;">Predictors</div>
+              <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">pred5</div>
+              <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-3.15</div>
+              <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">pred1</div>
+              <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-0.15</div>
+              <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">pred2</div>
+              <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-0.15</div>
+              <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">pred3</div>
+              <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-0.15</div>
+              <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">pred4</div>
+              <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-0.15</div>
+              <div class="col s12 left-align" style="font-family: monospace; margin-top:3px">Countries exposure</div>
+              <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">UK</div>
+              <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-0.15</div>
+              <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">DE</div>
+              <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-0.15</div>
+              <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">FR</div>
+              <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-0.15</div>
+              <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">ES</div>
+              <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-0.15</div>
+              <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">FI</div>
+              <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-0.15</div>
+              <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">SE</div>
+              <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-0.15</div>
+              <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">DK</div>
+              <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-0.15</div>
+              <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">NO</div>
+              <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-0.15</div>
+              <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">PT</div>
+              <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-0.15</div>
+              <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">GR</div>
+              <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-0.15</div>
+              <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">NL</div>
+              <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-0.15</div>
+              <div class="col s12 left-align" style="font-family: monospace; margin-top:3px">FX exposure</div>
+              <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">EUR</div>
+              <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-0.15</div>
+              <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">GBP</div>
+              <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-0.15</div>
+              <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">NOK</div>
+              <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-0.15</div>
+              <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">SEK</div>
+              <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-0.15</div>
+              <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">DKK</div>
+              <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-0.15</div>
+              <div class="col s12 left-align" style="font-family: monospace; margin-top:3px">Factors</div>
+              <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">Cash</div>
+              <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-0.15</div>
+              <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">Beta</div>
+              <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-0.15</div>
+            </div>
+          </div>
+        </div>
+      </li>
+
+
+      <li>
+          <div class="collapsible-header red darken-1 white-text text-darken-2 left-align"><b>STRAT2</b><span style="margin-left:20px">-0.30%</span></div>
+          <div class="collapsible-body">
+            <div class="container">
+              <div class="row">
+                <div class="col s12 left-align" style="font-family: monospace;">Predictors</div>
+                <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">pred1</div>
+                <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-3.15</div>
+                <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">pred2</div>
+                <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-0.15</div>
+                <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">pred3</div>
+                <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-0.15</div>
+                <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">pred4</div>
+                <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-0.15</div>
+                <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">pred5</div>
+                <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-0.15</div>
+                <div class="col s12 left-align" style="font-family: monospace; margin-top:3px">Countries exposure</div>
+                <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">UK</div>
+                <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-0.15</div>
+                <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">DE</div>
+                <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-0.15</div>
+                <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">FR</div>
+                <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-0.15</div>
+                <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">ES</div>
+                <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-0.15</div>
+                <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">FI</div>
+                <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-0.15</div>
+                <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">SE</div>
+                <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-0.15</div>
+                <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">DK</div>
+                <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-0.15</div>
+                <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">NO</div>
+                <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-0.15</div>
+                <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">PT</div>
+                <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-0.15</div>
+                <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">GR</div>
+                <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-0.15</div>
+                <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">NL</div>
+                <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-0.15</div>
+                <div class="col s12 left-align" style="font-family: monospace; margin-top:3px">FX exposure</div>
+                <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">EUR</div>
+                <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-0.15</div>
+                <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">GBP</div>
+                <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-0.15</div>
+                <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">NOK</div>
+                <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-0.15</div>
+                <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">SEK</div>
+                <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-0.15</div>
+                <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">DKK</div>
+                <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-0.15</div>
+                <div class="col s12 left-align" style="font-family: monospace; margin-top:3px">Factors</div>
+                <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">Cash</div>
+                <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-0.15</div>
+                <div class="col s2 grey lighten-1 white-text z-depth-2" style="margin-top:4px">Beta</div>
+                <div class="col s2 red darken-1 white-text text-darken-2 z-depth-2" style="margin-top:4px">-0.15</div>
+              </div>
+            </div>
+          </div>
+        </li>
+
+
+    </ul>
+</template>
+
+
+<style>
+.collapsible-body {
+  padding: 0px;
+}
+</style>
+
+<script>
+
+import JQuery from 'jquery'
+import M from 'materialize-css'
+
+let $ = JQuery
+
+export default {
+  name: 'Pnl',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+};
+
+$(document).ready(function(){
+  var elem = document.querySelector('.collapsible.expandable');
+  var instance = M.Collapsible.init(elem, {
+   accordion: false
+  });
+ });
+</script>
